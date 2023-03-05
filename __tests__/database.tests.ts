654d24856d33334510664d1e9db712c8f0a61e05
lionezajoao <joaopedrobzlioneza@hotmail.com>
2023-03-05 15:05:51 -0300

    Altered jest config

--- __tests__/database.tests.ts
+++ __tests__/database.tests.ts
@@ -1,16 +1,15 @@
 import Database from "../src/database";
+const db = new Database(process.env.MONGO_URI);
+
+beforeAll(async () => {
+    await db.connect();
+});
+
+afterAll(async () => {
+    await db.disconnect();
+});
 
 describe('Database', () => {
-    const db = new Database(process.env.MONGO_URI);
-
-    beforeAll(async () => {
-        await db.connect();
-    });
-
-    afterAll(async () => {
-        await db.disconnect();
-    });
-
     it('should connect to database', async () => {
         expect(db.isConnected()).toBeTruthy();
     });
